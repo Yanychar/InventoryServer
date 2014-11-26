b8a6919ed05704546f0a8dacd88b6cbd6c4904bd
Serguei Sevastianov <serguei.sevastianov@gmail.com>
2014-11-26 10:54:32 +0200

    Version info was added. Current version of Server: 1.0.1

---
+++ src/com/c2point/tools/Configuration.java
@@ -0,0 +1,14 @@
+package com.c2point.tools;
+
+public class Configuration {
+
+	private static int major_version = 1;
+	private static int mid_version = 0;
+	private static int minor_version = 1;
+	
+	
+	public static String getVersion() {
+		
+		return major_version + "." + mid_version + "." + minor_version;
+	}
+}
