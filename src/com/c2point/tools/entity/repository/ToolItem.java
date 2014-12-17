b8315047a7d0adea883376e69fe446fb261dc55d
Serguei Sevastianov <serguei.sevastianov@gmail.com>
2014-12-17 09:57:41 +0200

    "FreeStatusAllowed" property usage implementaiton
    Usage of ItemStatus.FREE now depends on company settings
    "FreeStatusAllowed"

--- src/com/c2point/tools/entity/repository/ToolItem.java
+++ src/com/c2point/tools/entity/repository/ToolItem.java
@@ -18,6 +18,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.joda.time.LocalDate;
 
+import com.c2point.tools.datalayer.SettingsFacade;
 import com.c2point.tools.entity.SimplePojo;
 import com.c2point.tools.entity.location.GeoLocation;
 import com.c2point.tools.entity.person.OrgUser;
@@ -154,8 +155,17 @@ public class ToolItem extends SimplePojo {
 		
 		setQuantity( 1 );
 
-		setStatus( ItemStatus.FREE );
+// Set to FREE if FREE status available in this organisation. Othervise INUSE
+		if ( tool != null ) {
+			
+			boolean freeAllowed = Boolean.parseBoolean(
+						SettingsFacade.getInstance().getProperty( tool.getOrg(), "FreeStatusAllowed", "false" ));
+			
+			setStatus( freeAllowed ? ItemStatus.FREE : ItemStatus.INUSE );
+	    	
+		}
 
+		
 		setPersonalFlag( false );
 	}
 
@@ -199,10 +209,22 @@ public class ToolItem extends SimplePojo {
 	protected Date getMaintenanceForDB() { return this.maintenanceForDB; }
 	protected void setMaintenanceForDB( Date maintenanceForDB ) { this.maintenanceForDB = maintenanceForDB; }	
 	
-	public LocalDate getBuyTime() { return LocalDate.fromDateFields( getBuyTimeForDB()); }
+	public LocalDate getBuyTime() { 
+		try {
+			return LocalDate.fromDateFields( getBuyTimeForDB()); 
+		} catch( Exception e ) {}
+		
+		return null;
+	}
 	public void setBuyTime( LocalDate date ) { setBuyTimeForDB( date != null ? date.toDate() : null ); }
 	
-	public LocalDate getMaintenance() { return LocalDate.fromDateFields( getMaintenanceForDB()); }
+	public LocalDate getMaintenance() {
+		try {
+			return LocalDate.fromDateFields( getMaintenanceForDB());
+		} catch( Exception e ) {}
+		
+		return null;
+	}
 	public void setMaintenance( LocalDate date ) { setMaintenanceForDB( date != null ? date.toDate() : null ); }
 
 	public Double getPrice() { return price; }
