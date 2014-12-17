aeb14e1223ff31989f9f2521582a94835097e597
Serguei Sevastianov <serguei.sevastianov@gmail.com>
2014-12-17 09:55:51 +0200

    Status change implementation in Repository View

---
+++ src/com/c2point/tools/ui/repositoryview/StatusSelector.java
@@ -0,0 +1,124 @@
+package com.c2point.tools.ui.repositoryview;
+
+import java.util.EventListener;
+
+import javax.swing.event.EventListenerList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.c2point.tools.InventoryUI;
+import com.c2point.tools.datalayer.SettingsFacade;
+import com.c2point.tools.entity.organisation.Organisation;
+import com.c2point.tools.entity.repository.ItemStatus;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.ui.OptionGroup;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+public class StatusSelector extends Window {
+	private static final long serialVersionUID = 1L;
+	private static Logger logger = LogManager.getLogger( StatusSelector.class.getName());
+
+	private Organisation org;
+	
+	public interface StatusSelectorListener extends EventListener {
+		
+		public void statusChanged( ItemStatus newStatus );
+	}
+	
+	private EventListenerList	listenerList = new EventListenerList();; 
+	
+	private OptionGroup single;
+	
+	public StatusSelector( Organisation org ) {
+		super();
+		
+		this.org = org;
+
+		initUI();
+		
+	}
+
+	public StatusSelector( Organisation org, ItemStatus status ) {
+		this( org );
+	
+		selectStatus( status );
+	}
+
+	private void initUI() {
+		
+		
+		VerticalLayout subContent = new VerticalLayout();
+		subContent.setMargin( true );
+		setContent( subContent );
+		setModal( true );
+//		setClosable( false );
+		setResizable( false );
+	
+//		center();
+		
+		boolean freeAllowed = Boolean.parseBoolean(
+				SettingsFacade.getInstance().getProperty( org, "FreeStatusAllowed", "false" ));
+		
+		freeAllowed = true;
+		
+		single = new OptionGroup();
+		for ( ItemStatus status : ItemStatus.values()) {
+			if ( status != ItemStatus.UNKNOWN 
+				 &&
+				 ( freeAllowed || !freeAllowed && status != ItemStatus.FREE )
+			) {
+				
+				single.addItem( status );
+				single.setItemCaption( status, status.toString((( InventoryUI )UI.getCurrent()).getSessionData().getBundle()));
+			}
+		}
+		
+		single.addValueChangeListener( new ValueChangeListener() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				
+				logger.debug( "ItemSelected: " + event.getProperty().getValue());
+
+				close();
+
+				fireStatusChanged(( ItemStatus )event.getProperty().getValue());
+				
+			}
+			
+		});
+		
+		subContent.addComponent( single );
+		
+	}
+
+	public void selectStatus( ItemStatus status ) {
+		
+		if ( status != null ) {
+			
+			single.select( status );
+		}
+	}
+	
+	public void addStatusChangedListener( StatusSelectorListener listener ) {
+		listenerList.add( StatusSelectorListener.class, listener);
+	}
+	
+	protected void fireStatusChanged( ItemStatus newStatus ) {
+		Object[] listeners = listenerList.getListenerList();
+
+	    for ( int i = listeners.length-2; i >= 0; i -= 2) {
+	    	if ( listeners[ i ] == StatusSelectorListener.class) {
+	    		(( StatusSelectorListener )listeners[ i + 1 ] ).statusChanged( newStatus );
+	         }
+	     }
+	 }
+
+
+	
+}
---
+++ src/com/c2point/tools/ui/repositoryview/handlers/ChangeStatusHandler.java
@@ -0,0 +1,85 @@
+package com.c2point.tools.ui.repositoryview.handlers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.c2point.tools.datalayer.ItemsFacade;
+import com.c2point.tools.entity.repository.ItemStatus;
+import com.c2point.tools.entity.repository.ToolItem;
+import com.c2point.tools.ui.repositoryview.StatusSelector;
+import com.c2point.tools.ui.repositoryview.ToolsListModel;
+import com.vaadin.server.Page;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.UI;
+
+public class ChangeStatusHandler extends AbstractHandler {
+
+	private static Logger logger = LogManager.getLogger( ChangeStatusHandler.class.getName());
+	
+	public ChangeStatusHandler( ToolsListModel model ) {
+		super( model );
+	}
+	
+	@Override
+	public ExitStatus handleCommand( final ToolItem item ) {
+
+		CommandListener.ExitStatus exitStatus = CommandListener.ExitStatus.UNKNOWN;
+
+		StatusSelector selector = new StatusSelector( getModel().getOrg());
+		
+		selector.selectStatus( item.getStatus());
+		
+		selector.addStatusChangedListener( new StatusSelector.StatusSelectorListener() {
+			
+			@Override
+			public void statusChanged( ItemStatus newStatus ) {
+
+				setNewStatus( item, newStatus );
+			}
+		});
+		
+		UI.getCurrent().addWindow( selector );
+		
+
+		exitStatus = CommandListener.ExitStatus.NONE;
+		
+		return exitStatus;
+	}
+
+	private CommandListener.ExitStatus setNewStatus( ToolItem item, ItemStatus newStatus ) {
+		
+		CommandListener.ExitStatus exitStatus = CommandListener.ExitStatus.FAILED_CHANGE;
+		
+		ToolItem updatedItem = ItemsFacade.getInstance().updateStatus( item, newStatus );
+		
+		if ( updatedItem != null ) {
+			if ( logger.isDebugEnabled()) logger.debug( "Specified Tool Item with Id=" + item.getId() + " has been updated."
+														+ " New status: " + newStatus );
+			
+			exitStatus = CommandListener.ExitStatus.STATUS_CHANGED;
+
+			new Notification( 
+					getModel().getApp().getResourceStr( "general.notify.header" ),
+					getModel().getApp().getResourceStr( "repositorymgmt.notify.statuschanged" ),
+					Notification.Type.HUMANIZED_MESSAGE, 
+					true 
+			).show( Page.getCurrent());
+			
+			getModel().fireToolChanged( updatedItem );
+			
+		} else {
+
+			new Notification( 
+					getModel().getApp().getResourceStr( "general.error.header" ),
+					getModel().getApp().getResourceStr( "repositorymgmt.error.statuschanged" ),
+					Notification.Type.ERROR_MESSAGE, 
+					true 
+			).show( Page.getCurrent());
+		}
+		
+		return exitStatus;
+		
+	}
+
+	
+}
