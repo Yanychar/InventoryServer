33bc3571932861d6772a6f0ab1ec3ddc219b2392
Yanychar <serguei.sevastianov@gmail.com>
2015-03-31 15:22:45 +0300

    Version 1.1.0 (version info added)

--- src/com/c2point/tools/Configuration.java
+++ src/com/c2point/tools/Configuration.java
@@ -14,6 +14,14 @@ import com.vaadin.server.VaadinService;
  *  1.0.4 (18.12.2014) - Bugs, Transactions backend, basic authorization (fake backend), model in filters
  *  1.0.5 (06.01.2015) - Client related Transaction Management. Personnel based view only
  *  1.0.6 (08.01.2015) - Client related Transaction Management
+ *  1.1.0 (31.03.2015) - Major release
+ *  Content of release:
+ *	- One account per several Companies (and even per several users in one company)
+ *	- Resource conversion tool was added
+ *	- Organisation Management
+ *	- Basic Access Rights Management
+ *	- "Forgot password" implemented with emailing it to the user or Service Owner
+ *	- New InventoryView with advanced filtering
  * 
  * 
  */
@@ -21,8 +29,8 @@ import com.vaadin.server.VaadinService;
 public class Configuration {
 
 	private static int major_version = 1;
-	private static int mid_version = 0;
-	private static int minor_version = 6;
+	private static int mid_version = 1;
+	private static int minor_version = 0;
 	
 	private static String	SYSTEM_CONFIG_DIR = "config";
 	private static String	SYSTEM_CONFIG_FILE = "config.properties";
